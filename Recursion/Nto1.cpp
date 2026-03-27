b86b3e7de516e68cb7028411fe0588481196c67b
Shiva Gupta <iitts.shiva@gmail.com>
2026-03-27 13:39:02 +0530

    Nto1 added

---
+++ Recursion/Nto1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printNumbers(int n){
+    if(n==0) return;
+    cout<<n<<endl;
+    printNumbers(n-1);
+
+}
+int main(){
+    int n;
+    cin>>n;
+    printNumbers(n);
+}
