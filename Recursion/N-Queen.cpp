e62e84ee6fad954b40443089949983fb0637e021
Shiva Gupta <iitts.shiva@gmail.com>
2026-04-04 16:06:48 +0530

    N-queen added

---
+++ Recursion/N-Queen.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isSafe(int row,int col,vector<string> board,int n){
+        int dupRow=row;
+        int dupCol=col;
+
+        while(row>=0 && col>=0){
+            if(board[row][col]=='Q') return false;
+            row--;
+            col--;
+        }
+
+        col=dupCol;
+        row=dupRow;
+        while(col>=0){
+            if(board[row][col]=='Q') return false;
+            col--;
+        }
+
+        col=dupCol;
+        row=dupRow;
+        while(row<n && col>=0){
+            if(board[row][col]=='Q') return false;
+            row++;
+            col--;
+        }
+
+        return true;
+    }
+
+    void solve(int col, vector<string>& board, vector<vector<string>>& ans, int n){
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+
+        for(int row=0; row<n; row++){
+            if(isSafe(row,col,board,n)){
+                board[row][col]='Q';
+                solve(col+1,board,ans,n);
+                board[row][col]='.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+
+        solve(0,board,ans,n);
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    int n = 4;  // change this value to test other sizes
+
+    vector<vector<string>> result = obj.solveNQueens(n);
+
+    cout << "Solutions for " << n << "-Queens:\n\n";
+
+    for (int k = 0; k < result.size(); k++) {
+        cout << "Solution " << k + 1 << ":\n";
+        for (auto &row : result[k]) {
+            cout << row << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
