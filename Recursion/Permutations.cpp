095db3941091d3ad2388261adb59759c815718d6
Shiva Gupta <iitts.shiva@gmail.com>
2026-04-04 12:53:31 +0530

    Permutations added

---
+++ Recursion/Permutations.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void f(vector<int>& ds, vector<int>& nums, vector<vector<int>>& ans, int freq[]) {
+        if (ds.size() == nums.size()) {
+            ans.push_back(ds);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (!freq[i]) {
+                ds.push_back(nums[i]);
+                freq[i] = 1;
+                f(ds, nums, ans, freq);
+                freq[i] = 0;
+                ds.pop_back();
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        int freq[nums.size()];
+        for (int i = 0; i < nums.size(); i++) freq[i] = 0;
+
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        f(ds, nums, ans, freq);
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {1, 2, 3};
+
+    vector<vector<int>> result = obj.permute(nums);
+
+    cout << "Permutations are:\n";
+    for (auto &vec : result) {
+        for (auto &x : vec) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
